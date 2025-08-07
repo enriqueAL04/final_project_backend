7df96afe2070a2180a748d1dc3cb56a32350b58c
Fabrizio <fabrizioaviles2016@gmail.com>
2025-08-08 00:06:45 +0200

    error

--- main.py
+++ main.py
@@ -20,5 +20,4 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"mensaje": "¡Hola desde FastAPI en Render!!"}
-
 app.include_router(users.router, prefix="/user", tags=["user-controller"])

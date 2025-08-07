61494ab15ba754e12b5af09dcf2d28cdc0cccad8
Fabrizio <fabrizioaviles2016@gmail.com>
2025-08-07 10:47:29 +0200

    All

--- app/api/users.py
+++ app/api/users.py
@@ -20,14 +20,14 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = create_user(db, user)
     return db_user
 
+@router.get("/all", response_model=List[UserOut])
+def read_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = db.query(User).offset(skip).limit(limit).all()
+    return users
+
 @router.get("/{id}", response_model=UserOut)
 def read_user(id: int, db: Session = Depends(get_db)):
     db_user = get_user(db, id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-@router.get("/all", response_model=List[UserOut])
-def read_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = db.query(User).offset(skip).limit(limit).all()
-    return users

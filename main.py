74a2c3ed4a7e3a89cdddb1286a01515ddb29c177
Fabrizio <fabrizioaviles2016@gmail.com>
2025-08-07 08:08:08 +0200

    Initial commit for final_project_backend

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⚠️ En producción cambia esto
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "¡Hola desde FastAPI en Render!"}

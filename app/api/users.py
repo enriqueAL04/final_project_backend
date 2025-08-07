fd9f786ba60867ceaeb76538b4900c4ed266990f
Fabrizio <fabrizioaviles2016@gmail.com>
2025-08-07 10:28:17 +0200

    Corregir errores

--- app/api/users.py
+++ app/api/users.py
@@ -23,8 +23,8 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 @router.get("/{id}", response_model=UserOut)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = get_user(db, user_id)
+def read_user(id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db, id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
